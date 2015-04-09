8e8c9fef6d239765e5a993eaa77d615704236c0d
Andre Torgal <andre.torgal@gmail.com>
2015-04-09 01:27:09 +0100

    first commit

---
+++ dist/lib/ng.cork.ui.js
@@ -0,0 +1,10 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('ng.cork.ui.stop-propagation', [
+        'ng.cork.ui.stop-propagation.enter-click',
+        'ng.cork.ui.stop-propagation.focus-on',
+        'ng.cork.ui.stop-propagation'
+    ]);
+
+})(angular);
---
+++ src/lib/ng.cork.ui.stop-propagation/stopPropagation.spec.js
@@ -0,0 +1,14 @@
+describe('ng.cork.ui.stop-propagation', function () {
+    'use strict';
+
+    beforeEach(module('ng.cork.ui.stop-propagation'));
+
+    describe('corkUiStopPropagation', function () {
+
+        it('should pass a dummy test.', inject(function () {
+            expect(true).toBe(true);
+        }));
+
+    });
+
+});
