701447ec54fee833c31d20d65008dfaa0eab7ba6
Andre Torgal <andre.torgal@gmail.com>
2015-04-09 01:35:35 +0100

    chore(release): v0.0.5

--- dist/ng.cork.ui.stop-propagation.js
+++ dist/ng.cork.ui.stop-propagation.js
@@ -1,5 +1,5 @@
 /**
- * ng.cork.ui.stop-propagation - v0.0.4 - 2015-04-09
+ * ng.cork.ui.stop-propagation - v0.0.5 - 2015-04-09
  * https://github.com/cork-labs/ng.cork.ui.stop-propagation
  *
  * Copyright (c) 2015 Cork Labs <http://cork-labs.org>
